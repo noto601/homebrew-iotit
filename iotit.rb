1565f47548c0dfd18a88c2e1d09ebe89b08f2304
goreleaserbot <goreleaser@carlosbecker.com>
2017-09-07 12:29:38 +0600

    iotit version v0.3.4

--- iotit.rb
+++ iotit.rb
@@ -1,9 +1,9 @@
 class Iotit < Formula
   desc "Go Language's command-line flashing tool for SBCs."
   homepage "https://github.com/xshellinc/iotit"
-  url "https://github.com/xshellinc/iotit/releases/download/v0.3.3/iotit_0.3.3_macos_amd64.tar.gz"
-  version "0.3.3"
-  sha256 "d782314b299e72b6072247006239e65bd7df08119c1b55e646087bb1455c57e0"
+  url "https://github.com/xshellinc/iotit/releases/download/v0.3.4/iotit_0.3.4_macos_amd64.tar.gz"
+  version "0.3.4"
+  sha256 "59c17270a90bb0084d3005753b61f95d7d47da944182b67477af311542e79d5d"
 
   def install
     bin.install "iotit"
