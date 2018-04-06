ef77ad8d6ead0fc0db35ab8c30f11c2aa0ae73ac
goreleaserbot <goreleaser@carlosbecker.com>
2018-04-06 15:44:51 +0900

    Brew formula update for iotit version v0.4.4

--- iotit.rb
+++ iotit.rb
@@ -1,15 +1,11 @@
 class Iotit < Formula
   desc "Go Language's command-line flashing tool for SBCs."
   homepage "https://github.com/xshellinc/iotit"
-  url "https://github.com/xshellinc/iotit/releases/download/v0.4.3/iotit_0.4.3_macos_amd64.tar.gz"
-  version "0.4.3"
-  sha256 "2e64e1f9062f9c4fdf8a7ba4ef643542661ca5ebf3d7110732a6481dca6f5a9b"
+  url "https://github.com/xshellinc/iotit/releases/download/v0.4.4/iotit_0.4.4_macos_amd64.tar.gz"
+  version "0.4.4"
+  sha256 "8eab5333e148c76cca42b54301407190ada728f6dfa7f20c4c53e91494ad527f"
 
   def install
     bin.install "iotit"
-  end
-
-  test do
-    
   end
 end
